ae7097b3b259d2effc308b75ca3ea290a74bd462
Jan Špaček <patek.mail@gmail.com>
2023-07-31 19:08:45 +0200

    Revert "fix doctest."
    This reverts commit cb2acfbd711712775f18e5842db478033b6f17c9.

--- src/decompress.rs
+++ src/decompress.rs
@@ -171,7 +171,6 @@ impl Decompressor {
     ///     width: header.width,
     ///     pad: 4, // for video
     ///     height: header.height,
-    ///     subsamp: header.subsamp,
     /// };
     ///
     /// // decompress the JPEG into the image
